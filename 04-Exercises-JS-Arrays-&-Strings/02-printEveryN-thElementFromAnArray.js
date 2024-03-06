7df430e5dd0797f5b07da8f139c4330173b7c9e7
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2024-03-06 23:35:20 +0200

    06.03.2024
    Exercises

---
+++ 04-Exercises-JS-Arrays-&-Strings/02-printEveryN-thElementFromAnArray.js
@@ -0,0 +1,13 @@
+function solve(numbers, steps) {
+    let stepsCounter = steps
+    let newArr = [] 
+
+   for (let i = 0; i < numbers.length; i+= steps) {
+    newArr.push(numbers[i])
+   }
+   return newArr
+}
+
+console.log(solve(['5', '20', '31', '4', '20'], 2))
+console.log(solve(['dsa', 'asd', 'test', 'tset'], 2))
+console.log(solve(['1', '2', '3', '4', '5'], 6))
---
+++ 04-Exercises-JS-Arrays-&-Strings/sorting-number-demo.js
@@ -0,0 +1,6 @@
+function solve(numbers){
+    console.log(numbers.sort((a, b) => a - b))
+
+}
+
+solve([1, 3, 10, 5, 64, 7])
