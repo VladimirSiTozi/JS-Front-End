1d043a5e92fae422a08f19aa314b8517db60f72a
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2024-04-04 23:22:21 +0300

    .

--- 10-DOM-Introduction-Exercises/Resources/07. Hell's Kitchen/app.js
+++ 10-DOM-Introduction-Exercises/Resources/07. Hell's Kitchen/app.js
@@ -2,7 +2,30 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
+      const inputText = document.querySelector('.restaurant-race-class #inputs textarea').value;
+      const pattern = /"([^"]*)"/g
+      const matches = inputText.matchAll(pattern)
+
+      for (const match of matches) {
+         console.log(match[1])
+         restaurant = {}
+         const [restaurantName, allEmployees] = match[1].split(' - ')
+         restaurant['rName'] = restaurantName
+         restaurant['totalSalary'] = 0
+         restaurant['bestSalary'] = 0
+
+         const employees = allEmployees.split(', ')
+         for (const employee of employees) {
+            const [employeeName, salary] = employee.split(' ')
+            restaurant[employeeName] = Number(salary)
+            restaurant['totalSalary'] += Number(salary)
+
+            if (salary > restaurant[bestSalary]) {
+               restaurant['bestSalary'] = salary
+            }
+         }
+
+      }
       
    }
 }
--- 10-DOM-Introduction-Exercises/Resources/08. Generate Report/report.js
+++ 10-DOM-Introduction-Exercises/Resources/08. Generate Report/report.js
@@ -1,3 +1,38 @@
 function generateReport() {
-    //TODO
+    const thElements = document.querySelectorAll('table thead th')
+    const trElements = document.querySelectorAll('table tbody tr')
+    const outputElement = document.getElementById('output')
+
+    const columns = Array
+        .from(thElements)
+        .map((thElement) => {
+            const checkboxElement = thElement.querySelector('input')
+            return {
+                name: thElement.textContent.toLocaleLowerCase().trim(),
+                active: checkboxElement.checked
+            };       
+    });
+
+    const reportArray = Array
+        .from(trElements)
+        .map(trElement => {
+            const tdElements = trElement.querySelectorAll('td') 
+        
+        const resultData = Array
+            .from(tdElements)
+            .reduce((result, tdElement, i) => {
+                if (!columns[i].active){
+                    return result
+                }
+                
+                const columnName = columns[i].name;
+                result[columnName] = tdElement.textContent
+
+                return result
+            }, {})
+
+        return resultData;
+        })
+
+    outputElement.value = JSON.stringify(reportArray, null, 2)
 }
