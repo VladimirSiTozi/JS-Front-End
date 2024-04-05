446398cf842b21e81b13b77d1e1922fee98a6689
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2024-04-06 00:22:07 +0300

    lab 2nd part

--- 11-DOM-Manipulation-and-Events/Resources/03. Add-Delete/app.js
+++ 11-DOM-Manipulation-and-Events/Resources/03. Add-Delete/app.js
@@ -1,3 +1,28 @@
 function addItem() {
-    //TODO...
+    const inputElement = document.getElementById('newItemText')
+    const ulElement = document.getElementById('items')
+
+    // create new intem
+    const newElement = document.createElement('li')
+    newElement.textContent = inputElement.value
+
+    // create link element
+    const deleteLinkElement = document.createElement('a')
+    deleteLinkElement.textContent = '[Delete]'
+    deleteLinkElement.href = '#'
+
+    // add event to link element
+    deleteLinkElement.addEventListener('click', () => {
+        newElement.remove()
+    })
+
+    //Append link element to the newElement
+    newElement.appendChild(deleteLinkElement)
+
+    // append to parent
+    ulElement.appendChild(newElement)
+
+    // clear input
+    inputElement.value = ''
 }
+
--- 11-DOM-Manipulation-and-Events/Resources/04. Mouse-Gradient/app.js
+++ 11-DOM-Manipulation-and-Events/Resources/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradienElement = document.getElementById('gradient')
+    const resultElement = document.getElementById('result')
+
+    gradienElement.addEventListener('mousemove', (event) => {
+        const currentWidth = event.offsetX
+        const elementWidth = event.target.clientWidth
+
+        const result = Math.floor((currentWidth / elementWidth) * 100)
+
+        resultElement.textContent = `${result}%`
+    });
 }
